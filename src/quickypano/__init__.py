5d0b28dd5dae958fe1a05a7b91591e416347cf57
Sybren A. Stüvel <sybren@stuvel.eu>
2014-09-12 23:21:45 +0200

    Lower scheduling priority class that's inherited by cpfind

--- src/quickypano/__init__.py
+++ src/quickypano/__init__.py
@@ -4,6 +4,8 @@ import ctypes
 
 PROCESS_MODE_BACKGROUND_BEGIN = 0x00100000
 PROCESS_MODE_BACKGROUND_END = 0x00200000
+IDLE_PRIORITY_CLASS = 0x00000040
+NORMAL_PRIORITY_CLASS = 0x00000020
 PROCESS_SET_INFORMATION = 0x0200
 
 kernel32 = ctypes.windll.kernel32
@@ -19,12 +21,12 @@ def lowpriority():
     """ Set the priority of the process to below-normal."""
 
     if sys.platform == 'win32':
-        _win_set_priority_class(PROCESS_MODE_BACKGROUND_BEGIN)
+        _win_set_priority_class(IDLE_PRIORITY_CLASS)
     else:
         os.nice(1)
 
 
 def normalpriority():
     if sys.platform == 'win32':
-        _win_set_priority_class(PROCESS_MODE_BACKGROUND_END)
+        _win_set_priority_class(NORMAL_PRIORITY_CLASS)
     # Unable to decrease nice level on other systems
